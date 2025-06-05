ca6c18fe80bf99568b560b6520546e61c3405c4e
Phuc Nguyen <186391026+phucfix@users.noreply.github.com>
2025-06-06 01:38:16 +0700

    Update 011_passengers.sql

--- migrations/v2/011_passengers.sql
+++ migrations/v2/011_passengers.sql
@@ -4,7 +4,7 @@ CREATE TABLE passengers (
     first_name TEXT,
     last_name TEXT,
     email TEXT CONSTRAINT email_check CHECK (email LIKE '%@%.%'),
-    phone_number BIGINT NOT NULL UNIQUE CONSTRAINT ph_length_check CHECK (LENGTH(phone_number::TEXT) = 10),
+    phone_number TEXT NOT NULL UNIQUE CONSTRAINT ph_length_check CHECK (LENGTH(phone_number) = 10),
     address TEXT,
     city TEXT,
     state TEXT,

7115abd81e7cece3ee9bb69405a17731aeb3721e
TrungHieu-alt <nguyentrunghieu24112004@gmail.com>
2025-06-06 09:46:29 +0700

    Add seat_number field and generation

--- backend/models/SeatDetail.js
+++ backend/models/SeatDetail.js
@@ -1,10 +1,12 @@
 class SeatDetail {
   constructor({
     id,
+    seat_number,
     travel_class_id,
     flight_id
   }) {
     this.id = id;
+    this.seat_number = seat_number;
     this.travel_class_id = travel_class_id;
     this.flight_id = flight_id;
   }
